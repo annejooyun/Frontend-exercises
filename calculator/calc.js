bbe937b4377a9056548b64806a2b7e19d568299f
Anne Joo Marthinsen <annejoomarthinsen@gmail.com>
2025-12-23 08:55:56 +0100

    Update logic: handeNumbers ok

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,75 +1,126 @@
+let buffer = "0";
+let lastOperator = null;
 
-let lastResult = 0;
-let currentInput = "";
-let lastbutton = null;
-
-// Operator buttons
-const clearButton = document.querySelector(".clear");
-const delButton = document.querySelector(".del");
-const divButton = document.querySelector(".div");
-const mulButton = document.querySelector(".mul");
-const subButton = document.querySelector(".sub");
-const addButton = document.querySelector(".add");
-const equalButton = document.querySelector(".equal");
+// Operator button
+const operatorButtons = document.querySelectorAll(".operator");
 
 // Number button
 const numberButtons = document.querySelectorAll(".number");
 
 // Number button event listener
-numberButtons.forEach(function(button) {
-    button.addEventListener("click", function() {
-        currentInput += Number(button.innerText);
-        document.querySelector(".result").innerText = currentInput;
-    });
+numberButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    handleNumber(Number(button.innerText));
+    updateResult();
+  });
 });
 
+// Update the result shown on the screen
+function updateResult() {
+  document.querySelector(".result").innerText = buffer;
+}
 
+//
+function handleNumber(value) {
+  if (buffer === "0") {
+    buffer = value.toString();
+  } else {
+    buffer += value.toString();
+  }
+}
+
+/*
 // Operator button event listeners
 clearButton.addEventListener("click", function () {
-    lastResult = 0;
-    currentInput = "";
-    lastButton = "clear";
-    document.querySelector(".result").innerText = lastResult;
+  lastResult = 0;
+  currentInput = "";
+  lastButton = "clear";
+  document.querySelector(".result").innerText = lastResult;
 });
 
 delButton.addEventListener("click", function () {
-    lastResult = Number(lastResult.toString().slice(0,-1));
-    lastButton = "del";
-    document.querySelector(".result").innerText = lastResult;
+  lastResult = Number(lastResult.toString().slice(0, -1));
+  lastButton = "del";
+  document.querySelector(".result").innerText = lastResult;
 });
 
 divButton.addEventListener("click", function () {
-    lastButton = "div";
+  lastButton = "div";
 });
 
 mulButton.addEventListener("click", function () {
-    lastButton = "mul";
-}); 
+  lastButton = "mul";
+});
 
 subButton.addEventListener("click", function () {
-    lastButton = "sub";
-    lastResult = Number(lastResult) - Number(currentInput);
-    currentInput = "";
-    document.querySelector(".result").innerText = lastResult;
+  lastButton = "sub";
+  lastResult = Number(lastResult) - Number(currentInput);
+  currentInput = "";
+  document.querySelector(".result").innerText = lastResult;
 });
 
 addButton.addEventListener("click", function () {
-    lastButton = "add";
-    lastResult = Number(lastResult) + Number(currentInput);
-        currentInput = "";
-    document.querySelector(".result").innerText = lastResult;
+  lastButton = "add";
+  lastResult = Number(lastResult) + Number(currentInput);
+  currentInput = "";
+  document.querySelector(".result").innerText = lastResult;
 });
 
 equalButton.addEventListener("click", function () {
-    if (lastButton === "add") {
-        lastResult = Number(lastResult) + Number(currentInput);
-        currentInput = "";
-    }
-    else if (lastButton === "sub") {
-            lastResult = Number(lastResult) - Number(currentInput);
-            currentInput = "";
-    }
+  if (lastButton === "add") {
+    lastResult = Number(lastResult) + Number(currentInput);
+    currentInput = "";
+  } else if (lastButton === "sub") {
+    lastResult = Number(lastResult) - Number(currentInput);
+    currentInput = "";
+  }
 
-    document.querySelector(".result").innerText = lastResult;
-    lastButton = "equal";
-}); 
+  document.querySelector(".result").innerText = lastResult;
+  lastButton = "equal";
+});
+
+
+function buttonClick(value) {
+  if (!isNaN(Number(value))) {
+    handleNumber(value);
+  } else {
+    handleOperator();
+  }
+  updateResult();
+}
+
+function handleNumber(value) {
+  if (lastResult === 0){
+    lastResult = value
+  }
+  else{
+    lastResult += value
+  }
+}
+
+function handleOperator(value) {
+  switch (value){
+    case "C":
+      lastResult = 0;
+
+    case "←":
+    case "÷":
+    case "x":
+    case "-":
+    case "+":
+    case "=":
+  }
+}
+
+function updateResult() {
+  document.querySelector(".result").innerText = lastResult;
+}
+
+function init() {
+  document.addEventListener("click", function () {
+    buttonClick(event.target.innerText);
+  });
+}
+
+init();
+*/
