c1102e86d7bf4965d0b329daa91b3b3586b9fda2
Anne Joo Marthinsen <annejoomarthinsen@gmail.com>
2026-01-05 21:34:46 +0100

    Working calculator!!

--- calculator/calc.js
+++ calculator/calc.js
@@ -32,163 +32,65 @@ function updateResult(value) {
 function handleNumber(value) {
   if (buffer === "0") {
     buffer = value.toString();
-    console.log("buffer is zero");
   } else {
     buffer += value.toString();
   }
 }
 
-function handleOperator(value) {
-  if (value === "C") {
+function handleOperator(pressedOperator) {
+  if (pressedOperator === "C") {
     buffer = "0";
     runningTotal = 0;
-    console.log("C button pressed. Buffer is", buffer);
-  } else if (value === "←") {
-    buffer = buffer.slice(0, -1);
-    console.log(buffer);
-  } else if (value === "=") {
-    handleMath();
-    buffer = "0";
+    lastOperator = null;
+    updateResult(buffer);
+    console.log("C button pressed. Everything is erased. Buffer is", buffer);
+  } else if (pressedOperator === "←") {
+    if (buffer.length < 2) {
+      buffer = "0";
+    } else {
+      buffer = buffer.slice(0, -1);
+    }
+    updateResult(buffer);
+    console.log("← button pressed. Last character erased. Buffer is", buffer);
+  } else if (pressedOperator === "=") {
+    console.log("I am in equal");
+    handleMath(pressedOperator);
     updateResult(runningTotal);
+    lastOperator = null;
+    runningTotal = 0;
+    buffer = "0";
   } else {
-    handleMath();
-    switch (value) {
-      case "÷":
-        console.log("Divide pressed");
-        lastOperator = "div";
-        break;
-      case "x":
-        console.log("Multiply pressed");
-        lastOperator = "mul";
+    handleMath(pressedOperator);
+    buffer = "0";
+    updateResult(buffer);
+    console.log("Buffer is", buffer, "Running total is", runningTotal);
+  }
+}
+
+function handleMath(pressedOperator) {
+  if (lastOperator === null) {
+    runningTotal += Number(buffer);
+    lastOperator = pressedOperator;
+    console.log("No lastOperator, so I set it to be", pressedOperator);
+  } else {
+    console.log("Last operator is", lastOperator);
+    switch (lastOperator) {
+      case "+":
+        runningTotal += Number(buffer);
+        lastOperator = pressedOperator;
         break;
       case "-":
-        console.log("Minus pressed");
-        lastOperator = "min";
-        break;
-      case "+":
-        console.log("Plus pressed");
-        lastOperator = "plu";
-        break;
-    }
-    runningTotal = Number(buffer);
-    console.log("runningtotal updated to", runningTotal);
-    buffer = "0";
-  }
-  updateResult(runningTotal);
-}
-
-function handleMath() {
-  if (buffer !== 0 && runningTotal !== 0) {
-    switch (lastOperator) {
-      case "plu":
-        runningTotal += Number(buffer);
-        console.log("running total is", runningTotal);
-        break;
-      case "min":
         runningTotal -= Number(buffer);
+        lastOperator = pressedOperator;
         break;
-      case "mul":
+      case "x":
         runningTotal *= Number(buffer);
+        lastOperator = pressedOperator;
         break;
-      case "div":
+      case "÷":
         runningTotal /= Number(buffer);
+        lastOperator = pressedOperator;
         break;
     }
   }
 }
-
-/*
-// Operator button event listeners
-clearButton.addEventListener("click", function () {
-  lastResult = 0;
-  currentInput = "";
-  lastButton = "clear";
-  document.querySelector(".result").innerText = lastResult;
-});
-
-delButton.addEventListener("click", function () {
-  lastResult = Number(lastResult.toString().slice(0, -1));
-  lastButton = "del";
-  document.querySelector(".result").innerText = lastResult;
-});
-
-divButton.addEventListener("click", function () {
-  lastButton = "div";
-});
-
-mulButton.addEventListener("click", function () {
-  lastButton = "mul";
-});
-
-subButton.addEventListener("click", function () {
-  lastButton = "sub";
-  lastResult = Number(lastResult) - Number(currentInput);
-  currentInput = "";
-  document.querySelector(".result").innerText = lastResult;
-});
-
-addButton.addEventListener("click", function () {
-  lastButton = "add";
-  lastResult = Number(lastResult) + Number(currentInput);
-  currentInput = "";
-  document.querySelector(".result").innerText = lastResult;
-});
-
-equalButton.addEventListener("click", function () {
-  if (lastButton === "add") {
-    lastResult = Number(lastResult) + Number(currentInput);
-    currentInput = "";
-  } else if (lastButton === "sub") {
-    lastResult = Number(lastResult) - Number(currentInput);
-    currentInput = "";
-  }
-
-  document.querySelector(".result").innerText = lastResult;
-  lastButton = "equal";
-});
-
-
-function buttonClick(value) {
-  if (!isNaN(Number(value))) {
-    handleNumber(value);
-  } else {
-    handleOperator();
-  }
-  updateResult();
-}
-
-function handleNumber(value) {
-  if (lastResult === 0){
-    lastResult = value
-  }
-  else{
-    lastResult += value
-  }
-}
-
-function handleOperator(value) {
-  switch (value){
-    case "C":
-      lastResult = 0;
-
-    case "←":
-    case "÷":
-    case "x":
-    case "-":
-    case "+":
-    case "=":
-  }
-}
-
-function updateResult() {
-  document.querySelector(".result").innerText = lastResult;
-}
-
-function init() {
-  document.addEventListener("click", function () {
-    buttonClick(event.target.innerText);
-  });
-}
-
-init();
-*/
