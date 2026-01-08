f283d3709878ac5f835c99b7bd20ff54c12b0c87
Anne Joo Marthinsen <annejoomarthinsen@gmail.com>
2026-01-08 20:14:30 +0100

    Added functionality for fetching word of the day from API

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -4,19 +4,41 @@ let numGuesses = 0;
 const maxLengthGuess = 5;
 const maxNumGuesses = 6;
 
+async function getWordOfTheDay() {
+  const promise = await fetch("https://words.dev-apis.com/word-of-the-day");
+  const processedResponse = await promise.json();
+  return processedResponse.word;
+}
+
+async function checkGuess() {
+  const word = await getWordOfTheDay();
+  console.log("Your guess", inputedGuess, "word", word);
+  return word === inputedGuess;
+}
+
+async function checkIsWord() {
+  //const promise = await
+}
+
 window.addEventListener("keyup", handleKeyUp);
 
-function handleKeyUp(event) {
+async function handleKeyUp(event) {
   const key = event.key;
   if (isLetter(key)) {
     if (inputedGuess.length < maxLengthGuess && numGuesses < maxNumGuesses) {
       inputedGuess += key;
       showLetter(key);
     }
+  } else if (key === "Backspace") {
+    removeLastLetter();
+    inputedGuess = inputedGuess.slice(0, -1);
   } else if (key === "Enter") {
     if (inputedGuess.length < maxLengthGuess) {
       console.log("The guess is not long enough. Insert a 5-letter word!");
     } else {
+      if (await checkGuess()) {
+        console.log("Congrats!!!");
+      }
       // check if guess is a word
       // check if guess is correct
       // include logic for checking for duplicates in guess, but not in answer
@@ -25,9 +47,6 @@ function handleKeyUp(event) {
       // numGuesses += 1;
       // maybe code hardmode?
     }
-  } else if (key === "Backspace") {
-    removeLastLetter();
-    inputedGuess = inputedGuess.slice(0, -1);
   }
 }
 
