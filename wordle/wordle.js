f3edfba35f2047e6e9de43fcc8575a9011e20505
Anne Joo Marthinsen <annejoomarthinsen@gmail.com>
2026-01-10 22:14:09 +0100

    Working Wordle program. Final logic + cleanup

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -5,6 +5,7 @@ const maxLengthGuess = 5;
 const maxNumGuesses = 6;
 
 const wordOfTheDayPromise = getWordOfTheDay();
+window.addEventListener("keyup", handleKeyUp);
 
 async function getWordOfTheDay() {
   const promise = await fetch("https://words.dev-apis.com/word-of-the-day");
@@ -14,8 +15,6 @@ async function getWordOfTheDay() {
 
 async function checkGuess() {
   const wordOfTheDayString = await wordOfTheDayPromise;
-  console.log("Your guess", inputedGuess);
-  console.log(typeof wordOfTheDayString);
   return wordOfTheDayString === inputedGuess;
 }
 
@@ -25,11 +24,8 @@ async function checkIsWord() {
     body: JSON.stringify({ word: inputedGuess }),
   });
   const processedResponse = await promise.json();
-  console.log(processedResponse);
   return processedResponse.validWord;
 }
-
-window.addEventListener("keyup", handleKeyUp);
 
 async function handleKeyUp(event) {
   const key = event.key;
@@ -43,24 +39,19 @@ async function handleKeyUp(event) {
     inputedGuess = inputedGuess.slice(0, -1);
   } else if (key === "Enter") {
     if (inputedGuess.length < maxLengthGuess) {
-      console.log("The guess is not long enough. Insert a 5-letter word!");
+      window.confirm("The guess is not long enough. Insert a 5-letter word!");
     } else {
       if (await checkGuess()) {
-        console.log("Congrats, you guessed it!!");
-        changeGuessColorsCorrect();
+        changeGuessColors([2, 2, 2, 2, 2]);
+        window.confirm("Congrats, you guessed it!!");
       } else {
         if (await checkIsWord()) {
-          console.log("Hmm not quite");
-          resultArray = await handleIncorrectGuess();
-
+          const resultArray = await handleIncorrectGuess();
+          changeGuessColors(resultArray);
           numGuesses += 1;
           inputedGuess = "";
         }
       }
-      // include logic for checking for duplicates in guess, but not in answer
-      // update colors for letters
-      //
-      // numGuesses += 1;
       // maybe code hardmode?
     }
   }
@@ -71,7 +62,6 @@ function showLetter(letter) {
   let col = String(inputedGuess.length);
 
   let className = ".box_" + row + "-" + col;
-  console.log(className);
   document.querySelector(className).innerText = letter.toUpperCase();
 }
 
@@ -87,27 +77,29 @@ function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 }
 
-function changeGuessColorsCorrect() {
-  // This function could recieve an array input instead, and change color of the letter positon depending on the array input
-  // 0: Letter is not in word, 1: Letter is in word, but at the wrong place, 2: Letter is in the correct spot
-  // F. ex., Word: HEARTH and guess: EARTH would give [1,1,2,2,1]
-  // And Word: NOVEL and guess: EARTH would give [1, 0, 0, 0]
-  // This would merge changeGuessColorsCorrect and changeGuessColorsIncorrect
+function changeGuessColors(resultArray) {
   for (let i = 1; i <= maxLengthGuess; i++) {
     row = numGuesses + 1;
     className = ".box_" + row + "-" + i;
-    document.querySelector(className).style.backgroundColor = "green";
-    document.querySelector(className).style.color = "white";
+    switch (resultArray[i - 1]) {
+      case 0:
+        break;
+      case 1:
+        document.querySelector(className).style.backgroundColor = "orange";
+        document.querySelector(className).style.color = "white";
+        break;
+      case 2:
+        document.querySelector(className).style.backgroundColor = "green";
+        document.querySelector(className).style.color = "white";
+        break;
+    }
   }
 }
-
-async function changeGuessColorsIncorrect() {}
 
 async function handleIncorrectGuess() {
   // Find number of correct letters in correct spaces
   // Find number of correct letters in incorrect spaces
   // Handle duplicates: guess: HOPPY and answer: PANTS should only give one yellow letter
-  //return ??
   const wordOfTheDayString = await wordOfTheDayPromise;
   let wordOfTheDayArray = wordOfTheDayString.split("");
 
@@ -116,21 +108,17 @@ async function handleIncorrectGuess() {
   for (let i = 0; i < maxLengthGuess; i++) {
     let letter = inputedGuess.charAt(i);
     if (wordOfTheDayArray.includes(letter)) {
-      console.log(letter, "in word", wordOfTheDayString);
       if (wordOfTheDayArray[i] === letter) {
         guessArray[i] = 2;
         wordOfTheDayArray[i] = "0";
-        console.log(wordOfTheDayArray);
       } else {
         guessArray[i] = 1;
-        wordOfTheDayArray[i] = "0";
-        console.log(wordOfTheDayArray);
+        const letterIndex = wordOfTheDayString.indexOf(letter);
+        wordOfTheDayArray[letterIndex] = "0";
       }
     } else {
-      console.log(letter, "not in word", wordOfTheDayString);
       guessArray[i] = 0;
     }
   }
-  console.log(guessArray);
   return guessArray;
 }
